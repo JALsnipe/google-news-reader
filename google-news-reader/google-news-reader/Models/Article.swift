3e7b6dba78b098027c4aa34a1292d53287133d28
Josh Lieberman <josh.lieberman92@gmail.com>
2015-07-26 12:09:38 -0400

    adding Article managed object

---
+++ google-news-reader/google-news-reader/Models/Article.swift
@@ -0,0 +1,39 @@
+//
+//  Article.swift
+//  google-news-reader
+//
+//  Created by Josh Lieberman on 7/26/15.
+//  Copyright © 2015 Lieberman. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import UIKit
+
+@objc(Article)
+class Article: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+    
+    convenience init(prototype: ArticlePrototype) {
+        
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        let entity = NSEntityDescription.entityForName("Article", inManagedObjectContext: appDelegate.managedObjectContext)!
+        
+        self.init(entity: entity, insertIntoManagedObjectContext: appDelegate.managedObjectContext)
+        
+        self.title = prototype.title
+        
+        self.articleDesciption = prototype.description
+        
+        self.imageURL = prototype.imageURL
+        self.date = prototype.date
+        self.articleURL = prototype.articleURL
+        
+        if let image = prototype.image as UIImage! {
+            self.image = image
+        }
+    }
+
+}
