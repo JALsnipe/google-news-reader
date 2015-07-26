083acc0e884a03d85019f8a2811b14114d0345af
Josh Lieberman <josh.lieberman92@gmail.com>
2015-07-25 23:30:15 -0400

    creating constants file, moving away from hardcoded values

---
+++ google-news-reader/google-news-reader/Supporting Files/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  google-news-reader
+//
+//  Created by Josh Lieberman on 7/25/15.
+//  Copyright © 2015 Lieberman. All rights reserved.
+//
+
+let kGoogleNewsRSSURL = "http://news.google.com/?output=rss"
+
+let kHTTPResponseStatusCodeSuccess = 200
