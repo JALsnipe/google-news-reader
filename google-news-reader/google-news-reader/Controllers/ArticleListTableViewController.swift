9111ac8c65e49300fa7dca2d42619a4ee808f191
Josh Lieberman <josh.lieberman92@gmail.com>
2015-07-25 23:20:55 -0400

    testing some basic ui, populating default UITableViewCells with article prototype content

---
+++ google-news-reader/google-news-reader/Controllers/ArticleListTableViewController.swift
@@ -0,0 +1,123 @@
+//
+//  ArticleListTableViewController.swift
+//  google-news-reader
+//
+//  Created by Josh Lieberman on 7/25/15.
+//  Copyright © 2015 Lieberman. All rights reserved.
+//
+
+import UIKit
+
+class ArticleListTableViewController: UITableViewController {
+    
+    var articleDataSource = [ArticlePrototype]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        NetworkManager().fetchAllArticlesWithCompletion { (data, error) -> Void in
+            if let unwrappedData = data as? NSData {
+                NetworkManager().parseAllArticleData(unwrappedData, completion: { (content, error) -> Void in
+                    print(content)
+                    print(error)
+                    
+                    if error == nil {
+                        if let unwrappedContent = content as [ArticlePrototype]! {
+                            self.articleDataSource = unwrappedContent
+                            
+                            // hop back on the main thread to reload, since we're in an async callback
+                            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                                self.tableView.reloadData()
+                            })
+                            
+                        }
+                    }
+                    
+                })
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.articleDataSource.count
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+
+        // Configure the cell...
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "cell")
+        
+        cell.textLabel?.text = self.articleDataSource[indexPath.row].title
+        cell.detailTextLabel?.text = self.articleDataSource[indexPath.row].description
+//        cell.imageView?.image =
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
